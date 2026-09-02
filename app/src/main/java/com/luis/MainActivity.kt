b500ff764b1f8feec500987ccfca3193171c733c
luis <luis.cucho@tecsup.edu.pe>
2026-09-02 17:04:05 -0500

    Agrega campos de ingreso con estado

--- app/src/main/java/com/luis/MainActivity.kt
+++ app/src/main/java/com/luis/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.luis
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.luis.ui.theme.Lab03RegistroProductoTheme
 
@@ -22,10 +29,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             Lab03RegistroProductoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    PantallaRegistro(
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
@@ -34,6 +44,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PantallaRegistro(modifier: Modifier = Modifier) {
+    var nombre by remember { mutableStateOf("") }
+    var precio by remember { mutableStateOf("") }
+    var cantidad by remember { mutableStateOf("") }
+    var mostrarResumen by remember { mutableStateOf(false) }
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -51,5 +65,53 @@ fun PantallaRegistro(modifier: Modifier = Modifier) {
         )
 
         Spacer(modifier = Modifier.height(24.dp))
+
+        // Campo nombre
+        OutlinedTextField(
+            value = nombre,
+            onValueChange = { nombre = it },
+            label = {
+                Text("Nombre del producto")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+
+        Spacer(
+            modifier = Modifier.height(16.dp)
+        )
+
+
+        // Precio y cantidad en una misma fila
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+
+            // Campo precio
+            OutlinedTextField(
+                value = precio,
+                onValueChange = { precio = it },
+                label = {
+                    Text("Precio (S/)")
+                },
+                modifier = Modifier.weight(1f)
+            )
+
+
+            Spacer(
+                modifier = Modifier.width(16.dp)
+            )
+
+
+            // Campo cantidad
+            OutlinedTextField(
+                value = cantidad,
+                onValueChange = { cantidad = it },
+                label = {
+                    Text("Cantidad")
+                },
+                modifier = Modifier.weight(1f)
+            )
+        }
     }
 }
